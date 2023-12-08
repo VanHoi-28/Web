b5c07e02988a28683d5d6040158220858085e445
VanHoi-28 <huynhvanhoi2811@gmail.com>
2023-12-08 23:54:18 +0700

    add

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,66 @@
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import React, { useCallback } from 'react';
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  //init
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, [])
+
+  const particlesLoaded = useCallback(async () => { }, []);
+
+  return (
+    <Particles
+      className='w-full h-full absolute translate-z-0'
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enabled: false },
+        background: {
+          color: {
+            value: '',
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enabled: false,
+              mode: 'push',
+            },
+            onHover: {
+              anable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#e68e2e',
+          },
+          links: {
+            color: '#f5d393',
+            distance: 150,
+            enabled: true,
+            opacity: 0.5,
+            width: 1
+          },
+        },
+      }}
+    />
+  );
 };
 
 export default ParticlesContainer;
